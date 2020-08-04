b87f11c764e9d85e55c16ab74d8fe12a3130fbb3
공선아 <glglthssla@naver.com>
2020-08-05 00:29:22 +0900

    [추가] LikeResult.java 좋아요 등록 결과 DTO 생성

---
+++ Ssafience/src/main/java/com/ssafy/ssafience/model/like/LikeResult.java
@@ -0,0 +1,11 @@
+package com.ssafy.ssafience.model.like;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LikeResult {
+	private boolean isThere;
+	private boolean isIn;	
+}
