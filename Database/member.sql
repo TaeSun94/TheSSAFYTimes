f0e0999b54e4f603cbdfc6c8e40e5ece9f2aa79a
김지웅 <kjw4611@gmail.com>
2020-08-04 21:38:57 +0900

    [수정] member의 카테고리 제약조건 | [추가] 팔로우 증가 트리거

--- Database/member.sql
+++ Database/member.sql
@@ -5,17 +5,22 @@
 */
 CREATE TABLE `member` (
 	`member_no` INT PRIMARY KEY AUTO_INCREMENT,
-	`member_id` VARCHAR(20) UNIQUE KEY NOT NULL,
-	`member_pw` VARCHAR(60) NOT NULL,
 	`member_email` VARCHAR(320) UNIQUE KEY NOT NULL,
+	`member_pw` VARCHAR(60) NOT NULL,
+	`member_id` VARCHAR(20) UNIQUE KEY ,
 	`member_first_name` VARCHAR(100),
 	`member_last_name` VARCHAR(100),
 	`member_phone` VARCHAR(32),
-	`member_region` INT DEFAULT 0,
-	`member_track` INT DEFAULT 0,
-	`member_unit` INT DEFAULT 0,
+	`member_region` INT,
+	`member_track` INT,
+	`member_unit` INT,
 	`member_intro` VARCHAR(500),
 	`member_desc` TEXT,
+    `member_article` INT NOT NULL DEFAULT 0,
+	`member_follow` INT NOT NULL DEFAULT 0,
+	`member_follower` INT NOT NULL DEFAULT 0,
+	`member_interested` TEXT ,
+	`member_skill` TEXT,
 	`member_auth` VARCHAR(20),
 	`member_auth_status` BOOL NOT NULL DEFAULT FALSE,
 	`member_auth_datetime` DATETIME,
@@ -27,7 +32,23 @@ CREATE TABLE `member` (
 
 );
 
-ALTER TABLE member ADD `member_class` INT DEFAULT 0;
+ALTER TABLE member MODIFY member_id VARCHAR(20) UNIQUE KEY;
+ALTER TABLE member DROP COLUMN member_class;
+
+START TRANSACTION ;
+ALTER TABLE member MODIFY member_region INT;
+ALTER TABLE member MODIFY member_unit INT;
+ALTER TABLE member MODIFY member_track INT;
+UPDATE member
+    SET member_region = 0,
+        member_unit = 0,
+        member_track = 0
+WHERE member_region IS NULL;
+ROLLBACK ;
+
+
+SELECT * FROM  member;
+
 /*============================================================================*/
 
 /*
@@ -117,5 +138,48 @@ FROM  member m
 SELECT m.*
 FROM  member m
     INNER JOIN (SELECT member_id_from FROM follow WHERE member_id_to = ?) f ON f.member_id_from = m.member_id;
+
+/*
+제작일: 2020.08.03
+유형: Trigger
+설명: 팔로우 시, 팔로우한 사람과 팔로우 된 사람의 팔로워 / 팔로우 수를 증가시킨다.
+ */
+DELIMITER //
+CREATE OR REPLACE TRIGGER TRG_FOLLOW_INCREMENT
+AFTER INSERT ON follow
+FOR EACH ROW
+    BEGIN
+        UPDATE member m
+            SET m.member_follow = (SELECT COUNT(f.member_id_to) FROM follow f WHERE f.member_id_from = NEW.member_id_from)
+        WHERE m.member_id = NEW.member_id_from;
+
+        UPDATE member m
+            SET m.member_follower = (SELECT COUNT(f.member_id_from) FROM follow f WHERE f.member_id_to = NEW.member_id_to)
+        WHERE m.member_id = NEW.member_id_to;
+    END //
+DELIMITER ;
+
+
+/*
+제작일: 2020.08.03
+유형: Trigger
+설명: 팔로우 해제 시, 팔로우한 사람과 팔로우 된 사람의 팔로워 / 팔로우 수를 감소시킨다.
+ */
+DELIMITER //
+CREATE OR REPLACE TRIGGER TRG_FOLLOW_DECREMENT
+AFTER DELETE ON follow
+FOR EACH ROW
+    BEGIN
+        UPDATE member m
+            SET m.member_follow = (SELECT COUNT(f.member_id_to) FROM follow f WHERE f.member_id_from = OLD.member_id_from)
+        WHERE m.member_id = OLD.member_id_from;
+
+        UPDATE member m
+            SET m.member_follower = (SELECT COUNT(f.member_id_from) FROM follow f WHERE f.member_id_to = OLD.member_id_to)
+        WHERE m.member_id = OLD.member_id_to;
+    END //
+DELIMITER ;
+
+
 
 /*============================================================================*/
