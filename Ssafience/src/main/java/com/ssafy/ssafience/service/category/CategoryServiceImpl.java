4570d10d3e3c37d76b41d06be555229fb590ae20
공선아 <glglthssla@naver.com>
2020-08-04 15:42:04 +0900

    [추가] CategoryService.java & CategoryServiceImpl.java 카테고리관련 Service 생성

---
+++ Ssafience/src/main/java/com/ssafy/ssafience/service/category/CategoryServiceImpl.java
@@ -0,0 +1,92 @@
+package com.ssafy.ssafience.service.category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ssafience.model.category.CategoryResult;
+import com.ssafy.ssafience.model.dto.Category;
+import com.ssafy.ssafience.repo.CategoryRepo;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+	
+	@Autowired
+	CategoryRepo repo;
+
+	@Override
+	public List<CategoryResult> getRegionList() throws Exception {
+		List<Category> list = repo.getRegionList();
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+
+	@Override
+	public List<CategoryResult> getUnitList(int categoryNo) throws Exception {
+		List<Category> list = repo.getUnitList(categoryNo);
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+
+	@Override
+	public List<CategoryResult> getTrackList(int categoryNo) throws Exception {
+		List<Category> list = repo.getTrackList(categoryNo);
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+	
+	@Override
+	public List<CategoryResult> getSkillAndLanguageList() throws Exception {
+		List<Category> list = repo.getSkillAndLanguageList();
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+	
+	@Override
+	public List<CategoryResult> getArticleList() throws Exception {
+		List<Category> list = repo.getArticleList();
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+	
+	@Override
+	public List<CategoryResult> getProjectList() throws Exception {
+		List<Category> list = repo.getProjectList();
+		List<CategoryResult> regionList = new ArrayList<CategoryResult>();
+		
+		for (Category cate : list) {
+			regionList.add(new CategoryResult(cate.getCategoryNo(), cate.getCategoryName()));
+		}
+		
+		return regionList;
+	}
+
+
+}
