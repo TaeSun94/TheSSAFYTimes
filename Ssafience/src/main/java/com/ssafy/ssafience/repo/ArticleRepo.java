43a1ae98064966899070ee8911672510c1c2fe9c
공선아 <glglthssla@naver.com>
2020-08-07 16:10:52 +0900

    [수정] AricleRepo.java 상세 조회 mapping 기능 추가

--- Ssafience/src/main/java/com/ssafy/ssafience/repo/ArticleRepo.java
+++ Ssafience/src/main/java/com/ssafy/ssafience/repo/ArticleRepo.java
@@ -7,11 +7,13 @@ import org.mapstruct.Mapper;
 import com.ssafy.ssafience.model.article.ArticleModifyRequest;
 import com.ssafy.ssafience.model.article.WriteRequest;
 import com.ssafy.ssafience.model.dto.Article;
+import com.ssafy.ssafience.model.dto.ArticleResultDTO;
 
 @Mapper
 public interface ArticleRepo {
-	public List<Article> selectArticleList();
-	public List<Article> selectMemberArticleList(String memberId);
+	public List<ArticleResultDTO> selectArticleList();
+	public List<ArticleResultDTO> selectMemberArticleList(String memberId);
+	public List<ArticleResultDTO> selectArticleDetailOne();
 	public Article selectArticleOne(int articleNo);
 	public int insert(WriteRequest request);
 	public int update(ArticleModifyRequest request);
