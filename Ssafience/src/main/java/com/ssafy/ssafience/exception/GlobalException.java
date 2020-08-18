e4c3be938e636d80d0cb602d7ef2859910c88e6d
glglthssla <glglthssla@naver.com>
2020-08-18 13:32:44 +0900

    [수정] /exception/ Exception Custom

---
+++ Ssafience/src/main/java/com/ssafy/ssafience/exception/GlobalException.java
@@ -0,0 +1,29 @@
+package com.ssafy.ssafience.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ssafience.model.BasicResponse;
+
+@ControllerAdvice
+@RestController
+public class GlobalException {
+	
+	static final String FAIL = "fail";
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	@ExceptionHandler(value = AccessDeniedException.class)
+	public ResponseEntity<BasicResponse> handleException(AccessDeniedException e){
+		final BasicResponse result = new BasicResponse();
+		System.out.println("-------------------------------------");
+		result.message = e.getMessage();
+		result.result = FAIL;
+		result.status = HttpStatus.UNAUTHORIZED;
+//		e.printStackTrace(); 
+		return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+	}
+}
---
+++ Ssafience/src/main/java/com/ssafy/ssafience/exception/JWTException.java
@@ -0,0 +1,5 @@
+package com.ssafy.ssafience.exception;
+
+public class JWTException extends Exception{
+
+}
