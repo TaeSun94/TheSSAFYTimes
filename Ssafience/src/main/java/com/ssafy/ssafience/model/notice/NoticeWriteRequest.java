28b3b83c55b5a11102ebdcca44e705de1ea53ef2
glglthssla <glglthssla@naver.com>
2020-08-18 13:25:26 +0900

    [수정] NoticeWriteRequest.java memberId Setter 추가

--- Ssafience/src/main/java/com/ssafy/ssafience/model/notice/NoticeWriteRequest.java
+++ Ssafience/src/main/java/com/ssafy/ssafience/model/notice/NoticeWriteRequest.java
@@ -1,7 +1,11 @@
 package com.ssafy.ssafience.model.notice;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Setter
 @Getter
 public class NoticeWriteRequest {
 	private String memberId;
