36e69fbce87c190272bade5efdd06a4424b6ac51
tyzlddy <tyzlddy@naver.com>
2020-08-19 15:07:03 +0900

    [수정] ProfileCard.vue 임시

--- Front/src/components/Profile/ProfileCard.vue
+++ Front/src/components/Profile/ProfileCard.vue
@@ -6,7 +6,7 @@
                 <div class="col-lg-3">
                     <connect-here v-model="member"></connect-here>
                 </div>
-                <div v-if="member.memberImgUrl" class="col-lg-6 text-center" style="position:relative;">
+                <!-- <div v-if="member.memberImgUrl" class="col-lg-6 text-center" style="position:relative;">
                     <v-avatar :size="150">
                         <img
                             :src="getImg(member.memberId)"
@@ -14,14 +14,16 @@
                         >  
                     </v-avatar>
                 </div>
-                <div v-else class="col-lg-6 text-center" style="position:relative;">
+                <div v-else class="col-lg-6 text-center" style="position:relative;"> -->
+                    <div class="col-lg-6 text-center" style="position:relative;">
                     <v-avatar :size="150">
                         <img
                             src="@/assets/마스크.jpg"
                             alt="John"
                         >  
                     </v-avatar>
-                </div>
+                    </div>
+                <!-- </div> -->
                 <div class="col-lg-3 text-center">
                     <!-- 만약 세션이 나이고 쿼리를 통해 들어온거와 같다면 -->
                     <div v-if="isMember">
@@ -211,9 +213,9 @@
                 const id = this.$cookies.get("memberId");
                 this.$store.dispatch('delFollowing',id);
             },
-            getImg(id){
-                return require("/home/ubuntu/ssafytimes/s03p13c208/Ssafience/src/main/resources/static/images/"+id+".jpg");
-            }
+            // getImg(id){
+            //     return require("/home/ubuntu/ssafytimes/s03p13c208/Ssafience/src/main/resources/static/images/"+id+".jpg");
+            // }
         }
     }
 </script>
